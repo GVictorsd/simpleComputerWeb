4fba16fb2e8a54862cf380e87c7998e2188d00e6
Victor <victorswaroop2002@gmail.com>
2021-11-30 23:10:35 +0530

    block diagram

--- main/views.py
+++ main/views.py
@@ -1,9 +1,17 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from django import forms
+
 def index(request):
+    print("hello world")
     return render(request, "main/index.html")
 
 def write(request):
-    print(request)
+    print(555555)
+    if request.method == 'POST':
+        add_code = request.POST.get('code', None)
+        if add_code is not None:
+            print(add_code)
